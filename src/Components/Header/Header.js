c3a005b2facffa8c118ffc5f12350a44b13f46d9
Arnob_007 <Arnob-007@users.noreply.github.com>
2020-10-18 04:20:42 +0600

    Good Practices

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -15,6 +15,8 @@ const Header = ({ setIP }) => {
 			<h2>IP Address Tracker</h2>
 			<div className='header__search'>
 				<input
+					aria-label='search'
+					name='ip_search'
 					placeholder='Enter IP'
 					type='text'
 					onChange={(e) => setIpTyping(e.target.value)}

b460052180d1c230f52ee425b621ca488f00162a
Arnob_007 <Arnob-007@users.noreply.github.com>
2020-10-18 04:04:12 +0600

    Updates

---
+++ precache-manifest.4def0c06d5cf430fa394581501d19449.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b49edcdf5af478c86ac9bb2cfd5b64",
+    "url": "/ip-tracker/index.html"
+  },
+  {
+    "revision": "09cce3ae6e48929801e9",
+    "url": "/ip-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "dc906d2182b78b0df006",
+    "url": "/ip-tracker/static/css/main.f885a695.chunk.css"
+  },
+  {
+    "revision": "09cce3ae6e48929801e9",
+    "url": "/ip-tracker/static/js/2.9474d611.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/ip-tracker/static/js/2.9474d611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc906d2182b78b0df006",
+    "url": "/ip-tracker/static/js/main.170c121b.chunk.js"
+  },
+  {
+    "revision": "bac147f6a83fad4c840f",
+    "url": "/ip-tracker/static/js/runtime-main.b1593b04.js"
+  },
+  {
+    "revision": "53ef8ed8ef4b92721f00cb8bfdc5c9b9",
+    "url": "/ip-tracker/static/media/icon-arrow.53ef8ed8.svg"
+  },
+  {
+    "revision": "ff17146919959b508ee5dd9af2f9f9e5",
+    "url": "/ip-tracker/static/media/icon-location.ff171469.svg"
+  },
+  {
+    "revision": "8d3f4ad64bf3a9320e172edf01e0c016",
+    "url": "/ip-tracker/static/media/pattern-bg.8d3f4ad6.png"
+  }
+]);
